113fb64ce7ec90bc5f27e0b6aac2893c586aebc6
YULI AVILEZ GAZCON <yuliavilezgazcon@gmail.com>
2024-09-13 20:39:36 -0500

    Proceso de creación del código.

---
+++ Punto1_Vendedor/Punto1_Vendedor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        double sueldoBase = 2000000, Comisión = 0.10, VlorTotalVenta, ValorVenta1, ValorVenta2,ValorVenta3;
+
+        Console.Write("Ingrese el valor de la primera venta: $");
+        ValorVenta1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la segunda venta: $");
+        ValorVenta2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la tercera venta: $");
+        ValorVenta3 = Convert.ToDouble(Console.ReadLine());
+
+        //Calculo de comisiones:
+        
+        Comisión * ValorVenta2;
+        Comisión * ValorVenta3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
