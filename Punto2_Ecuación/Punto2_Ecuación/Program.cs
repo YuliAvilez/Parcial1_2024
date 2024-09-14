87a5ba531a0c8c4369a9036b9d72525ad3cf00ab
YULI AVILEZ GAZCON <yuliavilezgazcon@gmail.com>
2024-09-13 22:31:29 -0500

    Fin del algoritmo.

--- Punto2_Ecuación/Punto2_Ecuación/Program.cs
+++ Punto2_Ecuación/Punto2_Ecuación/Program.cs
@@ -3,8 +3,8 @@
     static void Main(string[] args)
     {
      // Asignación de Variables
-     double a, b, x;
-     bool Solucion_Ecuación;
+     double a, b, x=0;
+     bool Solucion_Ecuación = false;
 
         //Pedir datos por teclado
         Console.Write("Por favor ingresa el valor de a: ");
@@ -13,6 +13,29 @@
         Console.Write("Por favor ingresa el valor de b: ");
         b = Convert.ToDouble(Console.ReadLine());
 
+        //Evaluación de los valores de X para determinar las soluciones de las ecuaciones
+        if (a != 0)
+        {
+            x = -b / a;
+            Solucion_Ecuación = true;
+        }
+        else
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("La ecuación tiene infinitas soluciones.");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no puede tener solución.");
+            }
+        }
 
+        if (Solucion_Ecuación)
+        {
+            Console.WriteLine($"La solución única de la ecuación es x = {x}");
+        }
     }
 }
+
+//Fin del Juego!!!
