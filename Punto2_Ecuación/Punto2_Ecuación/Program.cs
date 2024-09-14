e68cf82fbc3a50ff770a7a8e03add372c7cac5be
YULI AVILEZ GAZCON <yuliavilezgazcon@gmail.com>
2024-09-13 22:12:03 -0500

    Asignación de variables y pedir datos por teclado.

---
+++ Punto2_Ecuación/Punto2_Ecuación/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+     // Asignación de Variables
+     double a, b, x;
+     bool Solucion_Ecuación;
+
+        //Pedir datos por teclado
+        Console.Write("Por favor ingresa el valor de a: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingresa el valor de b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+
+    }
+}
